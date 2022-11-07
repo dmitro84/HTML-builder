4365ceb99f41bbf599e0acd65a28829f122e70ea
dmitry.kuchukov <kudim84@gmail.com>
2022-11-07 07:03:49 +0300

    fix: add process exit

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -8,17 +8,12 @@ const outputFile = fs.createWriteStream(path.join(__dirname, './dest.txt'));
 stdout.write('What is your name? Date of Birth?\n');
 stdin.on('data', (chunk)=> {
     
-    if(chunk.toString() === 'exit') {
-        // process.exit();
-        // console.log('exit');
-        chunk = '!';
+    if(chunk.toString().trim() === 'exit') {
+        process.exit();
     }else {
         outputFile.write(chunk);
     }
-   
-})
-
-
+   })
 
 process.on('exit', () => stdout.write('Good bye!\n'));
 
